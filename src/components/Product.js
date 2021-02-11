ad72568da7e5806f93c224d7b9c43767bfb227b7
syrine868 <syrine868@gmail.com>
2021-02-11 10:09:27 +0100

    First Component

---
+++ src/components/Product.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+import styled from "styled-components";
+export default class Product extends Component{
+    render(){
+        return <ProductFrame></ProductFrame>;
+    }
+}
+const ProductFrame=styled.div`
+    border-radius:25px;
+    min-height:150px;
+    min-width:150px;
+    background-color:rgb(110,110,0.7);
+    margin10px;`
+;
