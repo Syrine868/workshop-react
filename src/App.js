4b40b4b34f340bc79d8055ae7c066ac130b4bcd3
syrine868 <syrine868@gmail.com>
2021-02-11 11:48:15 +0100

    Update routes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom"; 
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
+import React, { Suspense } from "react";
 /**import product from "./product.json";
 import Product from "./components/Product";*/
 import styled from "styled-components";
 import Header from "./components/Header";
-import Welcome from "./pages/Welcome";
-import Products from "./pages/Products";
-import ProductDetails from "./pages/ProductDetails"; 
+const Welcome = React.lazy(() => import("./pages/Welcome"));
+const Products = React.lazy(() => import("./pages/Products"));
+const ProductDetails = React.lazy(() => import("./pages/ProductDetails"));
 
 function App() {
+  const connectedUser =undefined; 
+
   return (
     <>
       <Header> </Header>{" "}
       <AppFrame className="App">
-      <BrowserRouter basename="/">
+        <BrowserRouter basename="/">
+        <Suspense fallback={<p>...Loading page please wait</p>}>
+ {connectedUser ? (
  <Switch>
  <Route
  path="/welcome"
@@ -27,9 +32,39 @@ function App() {
  path="/product/:name"
  render={(props) => <ProductDetails {...props} />}
  ></Route>
- <Route exact render={() => <p>Default rendered page!</p>}></Route>
+ <Route
+ exact
+ render={() => (
+ <p>Default rendered page! Welcome {connectedUser.name}</p>
+ )}
+ ></Route>
  </Switch>
- </BrowserRouter>
+ ) : (
+ <Switch>
+ <Route
+ path="/welcome"
+ render={(props) => <Welcome {...props} />}
+ ></Route>
+ <Route
+ path="/login"
+ render={(props) => <Welcome {...props} />}
+ ></Route>
+ <Route
+ path="/products"
+ render={(props) => <Products {...props} />}
+ ></Route>
+ <Route
+ path="/product/:name"
+ render={(props) => <ProductDetails {...props} />}
+ ></Route>
+ <Route
+ exact
+ render={() => <p>You are not logged in!</p>}
+ ></Route>
+ </Switch>
+ )}
+ </Suspense>
+        </BrowserRouter>
       </AppFrame>
     </>
   );
